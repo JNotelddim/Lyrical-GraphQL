63084da5fdb9bef221c71c1004b869aaef7e2084
Jared Middleton <jaredmiddleton3.14@gmail.com>
2020-05-01 11:11:10 -0700

    Handle liking lyrics

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { graphql } from "react-apollo";
 
-const LyricList = ({ lyrics }) => {
+import likeLyricMutation from "../queries/likeLyric";
+
+const LyricList = ({ lyrics, mutate }) => {
+  const handleLike = (id) => {
+    mutate({
+      variables: { id },
+    });
+  };
+
   return (
     <ul className="collection">
-      {lyrics.map(({ id, content }) => (
+      {lyrics.map(({ id, content, likes }) => (
         <li key={id} className="collection-item">
           {content}
+          <div className="collection-item">
+            <span className="margin-m">{likes}</span>
+            <i className="material-icons" onClick={() => handleLike(id)}>
+              thumb_up
+            </i>
+          </div>
         </li>
       ))}
     </ul>
   );
 };
 
-export default LyricList;
+// lyric id5eab4b10d46b2207449ce987
+
+export default graphql(likeLyricMutation)(LyricList);
